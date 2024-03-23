c29f0ba0ca16e15808b1cba364f618fe53363c73
圆l <158799657+yuanzl77@users.noreply.github.com>
2024-03-23 08:33:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import config
-import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium_stealth import stealth
+import asyncio
 from bs4 import BeautifulSoup
 from utils import (
     getChannelItems,
@@ -24,6 +24,7 @@ logging.basicConfig(
     format="%(message)s",
     level=logging.INFO,
 )
+
 
 class UpdateSource:
 
@@ -49,62 +50,67 @@ class UpdateSource:
     def __init__(self):
         self.driver = self.setup_driver()
 
-    async def visit_page(self, cate, name, is_favorite):
-        channelUrls = {}
-        try:
-            for page in range(1, config.favorite_page_num if is_favorite else config.default_page_num):
-                page_url = f"http://tonkiang.us/?page={page}&s={name}"
-                self.driver.get(page_url)
-                self.driver_wait_for_element(By.CSS_SELECTOR, "div.tables")
-
-                soup = BeautifulSoup(self.driver.page_source, "html.parser")
-                tables_div = soup.find("div", class_="tables")
-                results = tables_div.find_all("div", class_="result") if tables_div else []
-                if not any(result.find("div", class_="m3u8") for result in results):
-                    break
-                infoList = []
-                for result in results:
-                    try:
-                        url, date, resolution = getUrlInfo(result)
-                        if url:
-                            infoList.append((url, date, resolution))
-                    except Exception as e:
-                        logging.error(f"Error on result {result}: {e}")
-                        continue
-                sorted_data = await compareSpeedAndResolution(infoList)
-                ipvSortedData = filterSortedDataByIPVType(sorted_data)
-                if ipvSortedData:
-                    channelUrls[name] = getTotalUrls(ipvSortedData) or channelUrls[name]
-                    for (url, date, resolution), response_time in ipvSortedData:
-                        logging.info(
-                            f"Name: {name}, URL: {url}, Date: {date}, Resolution: {resolution}, Response Time: {response_time}ms"
-                        )
-                else:
-                    channelUrls[name] = filterByIPVType(channelUrls[name])
-        except Exception as e:
-            logging.error(f"Error on category {cate} and name {name}: {e}")
-        updateChannelUrlsM3U(cate, channelUrls)
-        return cate, channelUrls
-
-    def driver_wait_for_element(self, by, value, timeout=10):
-        try:
-            WebDriverWait(self.driver, timeout).until(
-                EC.presence_of_element_located((by, value))
-            )
-        except Exception as e:
-            logging.error(f"Error waiting for element: {e}")
-
-    async def process_channels(self):
-        tasks = []
-        for cate, channelObj in getChannelItems().items():
+    async def visitPage(self, channelItems):
+        for cate, channelObj in channelItems.items():
+            channelUrls = {}
             for name in channelObj.keys():
-                is_favorite = name in config.favorite_list
-                tasks.append(self.visit_page(cate, name, is_favorite))
-        return await asyncio.gather(*tasks)
+                isFavorite = name in config.favorite_list
+                pageNum = (
+                    config.favorite_page_num if isFavorite else config.default_page_num
+                )
+                infoList = []
+                for page in range(1, pageNum):
+                    try:
+                        page_url = f"http://tonkiang.us/?page={page}&s={name}"
+                        self.driver.get(page_url)
+                        WebDriverWait(self.driver, 10).until(
+                            EC.presence_of_element_located(
+                                (By.CSS_SELECTOR, "div.tables")
+                            )
+                        )
+                        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+                        tables_div = soup.find("div", class_="tables")
+                        results = (
+                            tables_div.find_all("div", class_="result")
+                            if tables_div
+                            else []
+                        )
+                        if not any(
+                            result.find("div", class_="m3u8") for result in results
+                        ):
+                            break
+                        for result in results:
+                            try:
+                                url, date, resolution = getUrlInfo(result)
+                                if url:
+                                    infoList.append((url, date, resolution))
+                            except Exception as e:
+                                print(f"Error on result {result}: {e}")
+                                continue
+                    except Exception as e:
+                        print(f"Error on page {page}: {e}")
+                        continue
+                try:
+                    sorted_data = await compareSpeedAndResolution(infoList)
+                    ipvSortedData = filterSortedDataByIPVType(sorted_data)
+                    if ipvSortedData:
+                        channelUrls[name] = (
+                            getTotalUrls(ipvSortedData) or channelObj[name]
+                        )
+                        for (url, date, resolution), response_time in ipvSortedData:
+                            logging.info(
+                                f"Name: {name}, URL: {url}, Date: {date}, Resolution: {resolution}, Response Time: {response_time}ms"
+                            )
+                    else:
+                        channelUrls[name] = filterByIPVType(channelObj[name])
+                except Exception as e:
+                    print(f"Error on sorting: {e}")
+                    continue
+            updateChannelUrlsM3U(cate, channelUrls)
+            # await asyncio.sleep(1)
 
     def main(self):
-        self.driver = self.setup_driver()  # 创建Chrome Driver实例
-        asyncio.run(self.process_channels())
+        asyncio.run(self.visitPage(getChannelItems()))
         updateFile(config.final_file, "live_new.m3u")
         updateFile("result.log", "result_new.log")
 
